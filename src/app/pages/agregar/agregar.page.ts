84ab8194d2400af34681fcaa90b131d34c94b85e
Leonardo-Hurtado <145880163+Leonardo-Hurtado@users.noreply.github.com>
2024-11-04 18:21:09 -0300

    creacion de agregar

---
+++ src/app/pages/agregar/agregar.page.ts
@@ -0,0 +1,31 @@
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-agregar',
+  templateUrl: './agregar.page.html',
+  styleUrls: ['./agregar.page.scss'],
+})
+export class AgregarPage  {
+
+  nombreAct: string = '';
+  tipoAct: string = '';
+  detalleAct: string = '';
+  fechaAct: Date | null = null;
+  grupal: string = '';
+
+  constructor(private router: Router) { }
+
+  limpiarCampos() {
+    this.nombreAct = "";
+    this.tipoAct = "";
+    this.detalleAct = "";
+    this.fechaAct = null;
+    this.grupal = "";
+  }
+
+  siguiente() {
+    this.router.navigate(['/actividad'])
+  }
+
+}
